65dc84a267aa9d8f1d09e905067edade25351612
chenxudeng <1e1edeng>
2021-12-17 20:54:46 +0800

    added search bar

--- components/body.js
+++ components/body.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Search from "./search";
 
 function Body(props) {
     return (
-        <div>
-            body
-        </div>
+        <section className={'bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5'}>
+            {/*flexGrow take up as much space as possible*/}
+            <Search/>
+        </section>
     );
 }
 
---
+++ components/search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search(props) {
+    return (
+        <div className={'max-w-[1150px] bg-[#1a1a1a] rounded-full overflow-hidden border-2 border-[#333333] p-1.5 px-1.5 pr-8 flex item-center'}>
+            <div className={'h-4 w-4 rounded-full border-2 flex-shrink-0 animate-pulse'}/>
+        </div>
+    );
+}
+
+export default Search;
